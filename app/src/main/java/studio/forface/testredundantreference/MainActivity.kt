36d09b60535d5824b0561b0c8b5bb0547b88fca8
4face-studi0 <A37y91n47>
2019-06-06 19:19:50 +0200

    Initial commit

---
+++ app/src/main/java/studio/forface/testredundantreference/MainActivity.kt
@@ -0,0 +1,6 @@
+package studio.forface.testredundantreference
+
+import androidx.appcompat.app.AppCompatActivity
+
+// Do nothing, check `styles.xml` and `crash.xml`
+class MainActivity : AppCompatActivity()
